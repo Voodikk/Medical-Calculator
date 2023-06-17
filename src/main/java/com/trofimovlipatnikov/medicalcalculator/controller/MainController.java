16e88dbe433b5be99070998cd71cc974a67dc688
EkstazZz0 <128053305+EkstazZz0@users.noreply.github.com>
2023-06-17 21:29:21 +0400

    work

--- src/main/java/com/trofimovlipatnikov/medicalcalculator/controller/MainController.java
+++ src/main/java/com/trofimovlipatnikov/medicalcalculator/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.trofimovlipatnikov.medicalcalculator;
+package com.trofimovlipatnikov.medicalcalculator.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
